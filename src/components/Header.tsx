75d61cc53ea010ac9f400892507b6a412889de1d
anshsingh1121 <ansh24122002@gmail.com>
2025-03-30 18:24:32 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="mb-4 animate-fade-in">
-      <h1 className="text-2xl font-semibold text-gray-800">PocketPod</h1>
+      <h1 className="text-2xl font-semibold text-gray-800">SnapSound</h1>
     </header>
   );
 };
